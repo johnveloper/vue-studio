9509259695bd8e5eeaba706a4d81983cddd28176
johnveloper <johnveloper@gmail.com>
2019-04-08 02:21:09 +0200

    Make the picker and very simple navigation work (no history and back support)

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 let app = new Vue({
   el: '#app',
   data: {
-    route: 'home',
-    content: null
+    route: 'Home',
+    content: null,
+    pickerShown: false
   },
   mounted: function() {
-    axios.get('content/' + this.route + '.html').then(res => this.content = res.data);
+    this.show(this.route);
+  },
+  methods: {
+    togglePicker: function() {
+      this.pickerShown = !this.pickerShown;
+    },
+    show: function(route) {
+      this.route = route;
+      axios.get('content/' + route.toLowerCase() + '.html').then(res => this.content = res.data);
+    }
   }
 });
